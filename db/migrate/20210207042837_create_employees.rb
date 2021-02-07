ad6aa26052e956178eedd06193fca71af732d2d9
Swathidev <swathikarthic@outlook.com>
2021-02-07 04:39:37 +0000

    employee file

---
+++ db/migrate/20210207042837_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.string :designation
+
+      t.timestamps
+    end
+  end
+end
